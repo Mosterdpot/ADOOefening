00ca7725e1a81222a350b30549e4b2fed65efdbd
Mosterdpot <jeroen.merckx@gmail.com>
2016-05-23 09:00:12 +0200

    eerste commit

---
+++ Opgave01/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using OpgaveRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Opgave01
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonTest_Click(object sender, RoutedEventArgs e)
+        {
+            try 
+	        {	        
+		        var tuin = new TuinDb();
+                using (var conTuin = tuin.GetConnection())
+                { 
+                    //do stuff
+                    labelStatus.Content = "OK";
+                }
+            
+	        }
+	        catch (Exception ex)
+	        {
+                labelStatus.Content = ex.Message;
+		        
+	        }            
+
+        }
+    }
+}
---
+++ OpgaveRepository/TuinDb.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpgaveRepository
+{
+    public class TuinDb
+    {
+        private static ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings["Tuin"];
+        private static DbProviderFactory factory = DbProviderFactories.GetFactory(settings.ProviderName);
+
+        //public TuinDb()
+        //{
+
+        //}
+
+        public DbConnection GetConnection()
+        {
+            var conTuin = factory.CreateConnection();
+            conTuin.ConnectionString = settings.ConnectionString;
+            return conTuin;
+        }
+
+
+    }
+}
