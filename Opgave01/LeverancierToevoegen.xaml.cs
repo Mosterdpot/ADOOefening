9d27245dba2bda147fce67a80083a7c920ab07f1
Mosterdpot <jeroen.merckx@gmail.com>
2016-05-24 09:48:25 +0200

    window leverancier toevoegen

---
+++ Opgave01/LeverancierToevoegen.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using OpgaveRepository;
+
+namespace Opgave01
+{
+    /// <summary>
+    /// Interaction logic for LeverancierToevoegen.xaml
+    /// </summary>
+    public partial class LeverancierToevoegen : Window
+    {
+        public LeverancierToevoegen()
+        {
+            InitializeComponent();
+        }
+
+
+        private void Button_Toevoegen(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var tuin = new TuinManager();
+                var leverancier = new Leverancier()
+                {
+                    Naam = textNaam.Text,
+                    Adres = textAdres.Text,
+                    PostNr = textPostcode.Text,
+                    Woonplaats = textPlaats.Text
+                };
+                var id = tuin.LeverancierToevoegen(leverancier);
+                labelStatus.Content = "nieuwe leverancier toegevoegd met id=" + id.ToString();
+            }
+            catch (Exception ex)
+            {
+                labelStatus.Content = ex.Message;                
+            }
+        }
+
+        private void Button_Korting(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var manager = new TuinManager();
+                labelStatus.Content = manager.EindejaarsKorting().ToString() + " plantenprijzen aangepast";
+            }
+            catch (Exception ex)
+            {
+                labelStatus.Content = ex.Message;
+            }
+        }
+    }
+}
--- OpgaveRepository/Leverancier.cs
+++ OpgaveRepository/Leverancier.cs
@@ -8,6 +8,46 @@ namespace OpgaveRepository
 {
     public class Leverancier
     {
+        private int LevNrValue;
+        private String NaamValue;
+        private String AdresValue;
+        private String PostNrValue;
+        private String WoonplaatsValue;
+        public int LevNr
+        {
+            get
+            { return LevNrValue; }
+            set
+            { LevNrValue = value; }
+        }
+        public String Naam
+        {
+            get
+            { return NaamValue; }
+            set
+            { NaamValue = value; }
+        }
+        public String Adres
+        {
+            get
+            { return AdresValue; }
+            set
+            { AdresValue = value; }
+        }
+        public String PostNr
+        {
+            get
+            { return PostNrValue; }
+            set
+            { PostNrValue = value; }
+        }
+        public String Woonplaats
+        {
+            get
+            { return WoonplaatsValue; }
+            set
+            { WoonplaatsValue = value; }
+        }
     }
 
 }
